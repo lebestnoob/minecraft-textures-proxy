1cc9f2de77c1114829636ee136f5cfd53062b951
lebestnoob <lebestnoob12@gmail.com>
2022-07-23 12:06:16 -0700

    Use fetch and caching

--- README.md
+++ README.md
@@ -1,40 +1,45 @@
 # Minecraft Textures Proxy
-## A node app built to proxy Minecraft skins and capes.
+
+A node server built to proxy Minecraft skins and capes.
+
+### Setup:
+
+`git clone https://github.com/lebestnoob/minecraft-textures-proxy`
+
+`cd minecraft-textures-proxy`
+
+`npm install`
+
+`npm start` or `node server.js`
 
 ### Usage:
 
-The proxied version this provides shows the same results Mojang gives, with the exception of the session server. If you are retriving a skin from there, the texture URI will automatically be proxied too. 
-
-To access the sessionserver, send all requests to the root url, `domain.tld/{ANYTHING}`. For example, to obtain a players skin, the URI syntax would be  `domain.tld/session/minecraft/profile/{UUID}`. To retrieve Notch's skin, the URI would be  `domain.tld/session/minecraft/profile/069a79f444e94726a5befca90e38aaf5`. For more information on how to use the sessionserver, visit https://wiki.vg/Mojang_API.
-
-To access the Mojang API, prefix the URI with `/api/`. To get the UUID from a username, the URI would be `domain.tld/api/users/profiles/minecraft/{USERNAME}`. The response will be exactly the same as the one Mojang's API serves. This also allows POST requests via `https://domain.tld/profiles/minecraft`. For more information about the synatx of the Mojang API, visit https://wiki.vg/Mojang_API. Attempting to use this for authentication will not work. 
-
-This also has support for OptiFine's cape server and it automatically resizes the image to the standard 64x32 cape. To use it, prefix the URI with `/of/`. The URI would look like `https://domain.tld/of/{ANYTHING}`. To get a players OptiFine cape, load the URI like this `https://domain.tld/of/capes/{PROFILE}.png`. Notch's OptiFine cape would be `https://domain.tld/of/capes/NOTCH.png`.
-
-### Public Instance:
+**Every request is cached for **15** minutes to minimize rate-limiting.**
 
 #### Session Server:
 
-https://test.lebestnoob.repl.co
+Method GET: `/session/minecraft/profile/{UUID}`
 
-Get skin and cape, if the profile has one:
-https://test.lebestnoob.repl.co/session/minecraft/profile/{UUID}
+#### Texture Server:
+
+Method GET: `/texture/{hashCode}`
 
 #### Mojang API:
 
-GET: 
-https://test.lebestnoob.repl.co/api/users/minecraft/profile/{USERNAME}
+Method GET: `/api/users/profiles/minecraft/{USERNAME}`
 
-POST: 
-
-https://test.lebestnoob.repl.co/api/minecraft/profile
+Method POST: `/api/profiles/minecraft`
 
 #### OptiFine Cape:
 
-https://test.lebestnoob.repl.co/of/capes/{USERNAME}.png
+##### The server automatically resizes capes to a 64x32 resolution.
+
+Method GET: `/of/capes/{USERNAME}.png`
+
+### Public Instance:
+
+https://test.lebestnoob.repl.co
 
 ### Limitations:
-* Unable to show skin signature. 
-* Authentication will not work. I am not planning to implement this becuase it could lead to security issues and Mojang authentication will soon be phased out for the more secure Microsoft authentication.
-* Respsonse is slower than Mojang's own servers.
-* May be ratelimited as the request goes through your server.
+
+- OptiFine Special Cosmetics are not proxied
--- server.js
+++ server.js
@@ -1,271 +1,306 @@
 "use strict";
 
+// Imports
 import express from "express";
-import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+import compression from "compression";
+import apicache from "apicache-plus";
+import { fetchBuilder, MemoryCache } from "node-fetch-cache";
+const fetch = fetchBuilder.withCache(
+    new MemoryCache({
+        ttl: 900000, // 15 minutes
+    })
+);
+import { Headers, bag } from "fetch-headers";
 import sharp from "sharp";
-const app = express();
-const port = 3000;
 
-// Add headers: disable caching and other methods
-app.disable("x-powered-by");
+const app = express();
+const port = process.env.PORT || 3000;
+const REPL_PING = JSON.parse(process.env.REPL_PING) || false;
+const REPL_URL = process.env.REPL_URL;
+
+const headers = new Headers({ "Cache-Control": "s-maxage=900, max-age=900" });
+const options = { headers: headers, cache: "force-cache" };
+
+// Add response headers
 app.use((req, res, next) => {
-  res.append("Cache-Control", "no-store");
-  res.append("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  next();
+    res.set("Cache-control", "public, s-maxage=300, max-age=300");
+    res.append("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    next();
 });
 
 // Middleware
+app.use(compression());
+app.use(apicache("15 minutes"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("env", "production");
 app.set("json spaces", 2);
 
-// Disable stack trace on invalid POST.
-app.use(function (err, req, res, next) {
-  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-    res
-      .status(404)
-      .send({
-        error: "Not Found",
-        errorMessage:
-          "The server has not found anything matching the request URI",
-      });
-  } else next();
+// Disable stack trace on invalid requests.
+app.use(function(err, req, res, next) {
+    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+        res.status(404).send({
+            message: "Page Not Found",
+            path: req.path,
+        });
+    } else next();
 });
 
-// If /session/minecraft/profile is just sent, send a 404 error.
-app.get("/session/minecraft/profile/", async (req, res) => {
-  res.status(404).json({
-    message: "Page Not Found",
-    _links: {
-      self: {
-        href: req.path,
-        templated: false,
-      },
-    },
-    _embedded: {},
-  });
+app.get("/", async(req, res) => {
+    res.type("text/plain");
+    return res.sendStatus(200);
 });
 
-// Proxy all requests to /session/minecraft/profile/ and change the texture to a proxied version.
-app.get("/session/minecraft/profile/*", async (req, res) => {
-  const uri = `https://sessionserver.mojang.com${req.path}`;
+// If /session is sent, send status of session.mojang.com.
+app.get("/session/", async(req, res) => {
+    const uri = "https://sessionserver.mojang.com";
+    await fetch(uri, options)
+        .then((response) => {
+            return res.sendStatus(200);
+        })
+        .catch((error) => {
+            res.sendStatus(502);
+        });
+});
 
-  axios
-    .get(uri)
-    .then((response) => {
-      const data = response.data;
-      if ("error" in data) {
-        res.status(404).json(data);
-      } else {
-        let encoded = data.properties[0].value;
+// Proxy all requests to /session/minecraft/profile/ and change the texture to the proxied version.
+app.get("/session/minecraft/profile/:UUID", async(req, res) => {
+    const UUID = req.params.UUID;
+    const unsigned = req.query.unsigned;
+    let query = true;
+    if (unsigned === "false") query = false;
+    const uri = `https://sessionserver.mojang.com/session/minecraft/profile/${UUID}?unsigned=${query}`;
 
-        let decoded = Buffer.from(encoded, "base64").toString("utf8");
-        let data_1 = JSON.parse(decoded);
+    await fetch(uri, options)
+        .then((response) => response.json())
+        .then((data) => {
+            if ("error" in data) {
+                res.status(404).json(data);
+            } else {
+                let encoded = data.properties[0].value;
 
-        let image = data_1.textures.SKIN.url.replace(/^.*\/\/[^\/]+/, "");
+                let decoded = Buffer.from(encoded, "base64").toString("utf8");
+                let data_1 = JSON.parse(decoded);
 
-        data_1.textures.SKIN.url = `${req.protocol}://${req.get(
+                let image = data_1.textures.SKIN.url.replace(/^.*\/\/[^\/]+/, "");
+
+                data_1.textures.SKIN.url = `${req.protocol}://${req.get(
           "host"
-        )}/textures${image}`;
+        )}${image}`;
 
-        if ("CAPE" in data_1.textures) {
-          let image = data_1.textures.CAPE.url.replace(/^.*\/\/[^\/]+/, "");
+                if ("CAPE" in data_1.textures) {
+                    let image = data_1.textures.CAPE.url.replace(/^.*\/\/[^\/]+/, "");
 
-          data_1.textures.CAPE.url = `${req.protocol}://${req.get(
+                    data_1.textures.CAPE.url = `${req.protocol}://${req.get(
             "host"
-          )}/textures${image}`;
-        }
+          )}${image}`;
+                }
 
-        let string = JSON.stringify(data_1, null, 2);
+                let string = JSON.stringify(data_1, null, 2);
 
-        let fix = Buffer.from(string, "utf8").toString("base64");
+                let fix = Buffer.from(string, "utf8").toString("base64");
 
-        data.properties[0].value = fix;
+                data.properties[0].value = fix;
 
-        return res.json(data);
-      }
-    })
-    .catch((error) => {
-      res.status(404).json({
-        error: "Bad Request",
-        path: req.path,
-      });
-    });
-});
-
-// If an invalid /session path is sent, send Unauthorized error message.
-app.get("/session/*", async (req, res) => {
-  res.sendStatus(401);
-});
-
-// Show inccorrect method warning when attempting to load POST URI on web browser.
-app.get(
-  ["/api/profiles/minecraft", "/api/orders/statistics"],
-  async (req, res) => {
-    res
-      .status(404)
-      .json({
-        error: "Method Not Allowed",
-        errorMessage:
-          "The method specified in the request is not allowed for the resource identified by the request URI",
-      });
-  }
-);
-
-// Allow post request to /api/profiles/minecraft and return the original values from Mojang's API.
-app.post("/api/profiles/minecraft", (req, res) => {
-  axios
-    .post("https://api.mojang.com/profiles/minecraft", req.body)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      res.send("Error POSTing " + error);
-    });
-});
-
-// Allow post request to /api/orders/statistics and return the original values from Mojang's API.
-app.post("/api/orders/statistics", (req, res) => {
-  axios
-    .post("https://api.mojang.com/orders/statistics", req.body)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      res.send("Error POSTing " + error);
-    });
+                return res.json(data);
+            }
+        })
+        .catch((error) => {
+            res.status(404).json({
+                path: req.path,
+                errorMessage: `Not a valid UUID: ${UUID}`,
+                developerMessage: `Not a valid UUID: ${UUID}`,
+            });
+        });
 });
 
 // If /api is sent, send status of api.mojang.com.
-app.get("/api/", async (req, res) => {
-  axios
-    .get("https://api.mojang.com")
-    .then((response) => {
-      return res.sendStatus(200);
-    })
-    .catch((error) => {
-      res.sendStatus(502);
-    });
+app.get("/api/", async(req, res) => {
+    const uri = "https://api.mojang.com";
+    await fetch(uri, options)
+        .then((response) => {
+            return res.sendStatus(200);
+        })
+        .catch((error) => {
+            res.sendStatus(502);
+        });
 });
 
-// If anything in /api/ is sent, send a proxied version of api.mojang.com.
-app.get("/api/*", async (req, res) => {
-  axios
-    .get(`https://api.mojang.com${req.path.replace("/api", "")}`)
-    .then((response) => {
-      const html = response.data;
-      if ("" in html) {
-        res.sendStatus(404);
-      } else {
-        return res.json(html);
-      }
-    })
-    .catch((error) => {
-      res.sendStatus(404);
-    });
+// Show incorrect method warning when attempting to load POST URI on web browser.
+app.all("/api/profiles/minecraft", async(req, res) => {
+    if (req.method == "POST") {
+        const uri = "https://api.mojang.com/profiles/minecraft";
+        await fetch(uri, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(req.body),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                return res.json(data);
+            })
+            .catch((error) => {
+                return res.send("Error POSTing");
+            });
+    } else {
+        return res.status(405).json({
+            error: "Method Not Allowed",
+            errorMessage: "The method specified in the request is not allowed for the resource identified by the request URI",
+            path: req.path,
+        });
+    }
 });
 
-// If /textures is sent, send status of textures.minecraft.net.
-app.get("/textures/", async (req, res) => {
-  axios
-    .get("http://textures.minecraft.net")
-    .then((response) => {
-      return res.sendStatus(200);
-    })
-    .catch((error) => {
-      res.sendStatus(502);
-    });
+// Allow post request to /api/profiles/minecraft and return the original values from Mojang's API.
+// app.post("/api/profiles/minecraft", async(req, res) => {});
+
+// Send a proxied version of api.mojang.com/users/profiles/minecraft/
+app.get("/api/users/profiles/minecraft/:username", async(req, res) => {
+    const user = req.params.username;
+    const at = parseInt(req.query.at);
+    let timestamp = null;
+    if (Number.isInteger(at)) timestamp = at;
+    const uri = `https://api.mojang.com/users/profiles/minecraft/${user}?at=${timestamp}}`;
+
+    await fetch(uri, options)
+        .then((response) => response.json())
+        .then((data) => {
+            // const html = response.data;
+            if ("" in data) {
+                res.sendStatus(404);
+            } else {
+                return res.json(data);
+            }
+        })
+        .catch((error) => {
+            res.sendStatus(404);
+        });
 });
 
-// If anything in /textures/ is sent, send a proxied version of textures.minecraft.net.
-app.get("/textures/*", async (req, res) => {
-  axios
-    .get(`http://textures.minecraft.net${req.path.replace("/textures", "")}`, {
-      responseType: "arraybuffer",
-    })
-    .then((response) => {
-      res.append("Content-Type", "image/png");
-      return res.send(Buffer.from(response.data));
-    })
-    .catch((error) => {
-      res.append("Content-Type", "text/plain");
-      res.sendStatus(404);
-    });
+// Send a proxied version of api.mojang.com/user/profiles/uuid/names.
+app.get("/api/user/profiles/:UUID/names", async(req, res) => {
+    const UUID = req.params.UUID;
+    const uri = `https://api.mojang.com/user/profiles/${UUID}/names`;
+
+    await fetch(uri, options)
+        .then((response) => response.json())
+        .then((data) => {
+            // const html = response.data;
+            if ("" in data) {
+                res.sendStatus(404);
+            } else {
+                return res.json(data);
+            }
+        })
+        .catch((error) => {
+            res.sendStatus(404);
+        });
+});
+
+// If /texture is sent, send status of textures.minecraft.net.
+app.get("/texture/", async(req, res) => {
+    const uri = "http://textures.minecraft.net";
+
+    await fetch(uri, options)
+        .then((response) => {
+            return res.sendStatus(200);
+        })
+        .catch((error) => {
+            res.sendStatus(502);
+        });
+});
+
+// Send a proxied version of textures.minecraft.net/texture/.
+app.get("/texture/:hashCode", async(req, res) => {
+    const hashCode = req.params.hashCode;
+    const uri = `http://textures.minecraft.net/texture/${hashCode}`;
+
+    await fetch(uri, options)
+        .then((response) => response.body)
+        .then((response) => {
+            res.type("png");
+            return response.pipe(res);
+        })
+        .catch((error) => {
+            res.type("text/plain");
+            res.sendStatus(404);
+        });
 });
 
 // If /of is sent, send status of s.optifine.net.
-app.get("/of/", async (req, res) => {
-  axios
-    .get("http://s.optifine.net")
-    .then((response) => {
-      return res.sendStatus(200);
-    })
-    .catch((response, error) => {
-      return res.sendStatus(502);
-    });
-});
+app.get("/of/", async(req, res) => {
+    const uri = "http://s.optifine.net";
 
-// If anything in /of/ is sent, send a proxied version of s.optifine.net.
-app.get("/of/*", async (req, res) => {
-  axios
-    .get(`http://s.optifine.net${req.path.replace("/of", "")}`, {
-      responseType: "arraybuffer",
-    })
-    .then((response) => {
-      // Resize Optifine cape to correct dimmensions.
-      sharp(Buffer.from(response.data))
-        .resize(64, 32, {
-          withoutEnlargement: true,
+    await fetch(uri, options)
+        .then((response) => {
+            return res.sendStatus(200);
         })
-        .extend({
-          bottom: 10,
-          right: 18,
-          background: { r: 0, g: 0, b: 0, alpha: 0 },
+        .catch((response, error) => {
+            return res.sendStatus(502);
+        });
+});
+
+// Ssend a proxied version of s.optifine.net/capes.
+app.get("/of/capes/:username.png", async(req, res) => {
+    const user = req.params.username;
+    const uri = `http://s.optifine.net/capes/${user}.png`;
+
+    await fetch(uri, options)
+        .then((response) => {
+            if (response.ok) {
+                return response.body;
+            }
+            return Promise.reject(response);
         })
-        .png()
-        .toBuffer()
-        .then((data) => res.type("png").send(data));
-    })
-    .catch((error) => {
-      res.append("Content-Type", "text/plain");
-      res.sendStatus(404);
-    });
+        .then((data) => {
+            // Resize OptiFine cape to correct dimensions.
+            const OFCapeResizer = sharp()
+                .resize(46, 22, {
+                    kernel: sharp.kernel.nearest,
+                    fit: sharp.fit.contain,
+                    background: { r: 0, g: 0, b: 0, alpha: 0 },
+                    withoutEnlargement: true,
+                })
+                .extend({
+                    bottom: 10,
+                    right: 18,
+                    background: { r: 0, g: 0, b: 0, alpha: 0 },
+                })
+                .png();
+            res.type("png");
+            return data.pipe(OFCapeResizer).pipe(res);
+        })
+        .catch((error) => {
+            res.type("text/plain");
+            return res.sendStatus(404);
+        });
 });
 
-// Proxy all root url requests to Mojang's session server.
-app.get("/", async (req, res) => {
-  res.append("Content-Type", "text/plain");
-  res.sendStatus(200);
-});
-
-// Proxy all root url requests to Mojang's session server.
-app.get("/*", async (req, res) => {
-  res.append("Content-Type", "text/plain");
-  axios
-    .get(`https://sessionserver.mojang.com${req.path}`)
-    .then((response) => {
-      const html = response.data;
-      return res.send(html);
-    })
-    .catch((error) => {
-      res.sendStatus(404);
+// 404 error
+app.use(async(req, res) => {
+    res.status(404).json({
+        message: "Page Not Found",
+        path: req.path,
     });
 });
 
 // Start Express.JS server.
-app.listen(port, function () {
-  console.log(`Listening on port ${port}!`);
-});
+const server = app.listen(port, () =>
+    console.log(`Listening on port ${port}!`)
+);
+server.setTimeout(5000);
 
 // Keep server alive for atleast 12 hours
-setInterval(async () => {
-  await axios
-    .get(
-      "https://test.lebestnoob.repl.co/" // Change this to your project url if using Repl.it, Glitch.me, or a similar service to keep it online.
-    )
-    .then(console.log("Alive!"))
-    .catch((error) => {
-      console.log("Alive!");
-    });
-}, 240000);
+if (REPL_PING) {
+    setInterval(async() => {
+        await fetch(REPL_URL)
+            .then(console.log("Alive!"))
+            .catch((error) => {
+                console.log("Alive!");
+            });
+    }, 240000);
+}
